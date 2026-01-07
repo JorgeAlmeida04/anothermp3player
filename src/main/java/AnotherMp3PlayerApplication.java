c6404c2110c219484b2856224980d0053007e311
Jorge Martinho Ferreira Almeida <1220838@isep.ipp.pt>
2026-01-07 21:29:40 +0000

    First commit to set up all the repo

---
+++ src/main/java/AnotherMp3PlayerApplication.java
@@ -0,0 +1,13 @@
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnotherMp3PlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnotherMp3PlayerApplication.class, args);
+	}
+
+}
