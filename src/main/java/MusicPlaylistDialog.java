9f32731e66c7b739becb816ce9da1b7fa21f3501
Jorge Martinho Ferreira Almeida <1220838@isep.ipp.pt>
2026-01-23 10:30:33 +0000

    feat: Added playlist creation

--- src/main/java/MusicPlaylistDialog.java
+++ src/main/java/MusicPlaylistDialog.java
@@ -3,7 +3,9 @@ import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
 import java.util.ArrayList;
 
 public class MusicPlaylistDialog extends JDialog {
@@ -73,7 +75,45 @@ public class MusicPlaylistDialog extends JDialog {
         savePlaylistButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
+                try{
+                    JFileChooser filePicker = new JFileChooser();
+                    filePicker.setCurrentDirectory(new File("src/main/java/assets"));
+                    int result = filePicker.showSaveDialog(MusicPlaylistDialog.this);
 
+                    if(result == JFileChooser.APPROVE_OPTION){
+                        //Use getSelectedFile() to get reference to the file that we are about to save
+                        File selectedFile = filePicker.getSelectedFile();
+
+                        //Convert to .txt file if not done so already
+                        //Check to see if the file does not have the ".txt" file extension
+                        if(!selectedFile.getName().substring(selectedFile.getName().length() - 4).equalsIgnoreCase(".txt")){
+                            selectedFile = new File(selectedFile.getAbsoluteFile() + ".txt");
+                        }
+
+                        //Create the new file at the destinated directory
+                        selectedFile.createNewFile();
+
+                        //Write all the song paths into the file
+                        FileWriter fw = new FileWriter(selectedFile);
+                        BufferedWriter bufferedWriter = new BufferedWriter(fw);
+
+                        //Iterate through the song paths list and write each string into the file
+                        //Each song will be written in their own row
+                        for(String songPath : songPaths){
+                            bufferedWriter.write(songPath + "\n");
+                        }
+                        bufferedWriter.close();
+
+                        //Display success dialog
+                        JOptionPane.showMessageDialog(MusicPlaylistDialog.this, "Successfully Created Playlist!");
+
+                        //Close dialogue
+                        MusicPlaylistDialog.this.dispose();
+
+                    }
+                } catch(Exception ex){
+                    throw new RuntimeException(ex);
+                }
             }
         });
         add(savePlaylistButton);
